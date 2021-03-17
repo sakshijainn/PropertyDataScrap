dc43350a07c5821e813022508037180b5fa6b182
sakshijainn <sakshijjain1997@gmail.com>
2021-03-17 13:04:38 +0530

    update with multiple urls

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const puppeteer = require("puppeteer");
-const fs  = require("fs");
-const json2csv = require("json2csv").Parser;
+// const fs  = require("fs");
+// const json2csv = require("json2csv").Parser;
 const {google} = require('googleapis');
 const keys= require("./keys.json");
 
-
-
+let propertyData =[];
+let dataObtained =[];
+let dataObtainedArray=[];
+let dataObtainedArray1D =[];
 const client  = new google.auth.JWT(
     keys.client_email, 
     null, 
@@ -25,10 +27,28 @@ client.authorize(function(err,tokens){
     }
 
 })
-
+ 
 async function gsrun(cl)
 {
     const gsapi = google.sheets({version:'v4', auth:cl});
+    const opt ={
+        spreadsheetId : '1HWQgo0RiXy98DizjDfFb12LP5aXK5LiuCSDoyc8kMnc',
+        range: "Sheet1!A3:A8"
+        
+
+    };
+    
+
+    dataObtained= await gsapi.spreadsheets.values.get(opt);
+    console.log(dataObtained);
+    dataObtainedArray = dataObtained.data.values;
+    console.log(dataObtainedArray);
+    dataObtainedArray1D = [].concat(...dataObtainedArray);
+    console.log(dataObtainedArray1D);
+  
+
+
+   
     const updateopt ={
         spreadsheetId : '1vE8q7eGAn3tdivMGle1pLSNGyB8uuGm8wWUjbS5KsOk',
         range: "Sheet1!A2",
@@ -43,7 +63,6 @@ async function gsrun(cl)
     console.log(res);
 }
 
-let propertyData =[];
 
 (async function main(){
 
@@ -54,7 +73,13 @@ let propertyData =[];
         page.setUserAgent(
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
            )
-        await page.goto('https://www.smartpropertyinvestment.com.au/data/nsw/2075/st-ives')
+        
+        for (const url of dataObtainedArray1D)
+        {
+      
+        await page.goto(url)
+        
+       
 
         const data = await page.$$eval('table tr td', tds => tds.map((td) => {
             return td.innerText;
@@ -74,21 +99,17 @@ let propertyData =[];
         const weeklyHouseHoldIncome = res[1].trim(); //Weekly Household Income
         const householdSize  = res[2].trim(); //House Hold Size
 
-        propertyData.push({
-            MedianPrice,
+        propertyData.push([ MedianPrice,
             House,
             Unit,
             totalPopulation,
             weeklyHouseHoldIncome,
-            householdSize
-        })
+            householdSize]);
+            console.log(propertyData);
+            propertyData=[];
 
-        console.log(propertyData);
-        const json2csvParser = new json2csv();
-        const csv = json2csvParser.parse(propertyData);
-
-        fs.writeFileSync("./csv/data.xlsx",csv,"utf-8");
-
+        
+        }
         
 
     }
@@ -98,4 +119,4 @@ let propertyData =[];
     }
 
 })();
- 
+
